0e7190c1e85affe08c7907a7cb697fd440fc5871
aglipanci <aglipanci@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 22:12:45 +0000

    fixing https and pull path
    git-svn-id: https://plugins.svn.wordpress.org/chrome-push-notifications/trunk@1159200 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-chrome-push.php
+++ includes/class-chrome-push.php
@@ -53,8 +53,8 @@ class WPChromePush {
 	 */
 	public static function writeServiceWorker() {
 		$tmp_sw = file_get_contents(CHROME_PUSH_PLUGIN_DIR . 'assets/js/tmp/sw.js.tmp');
-	    $tmp_sw = str_replace('DEXIE_PATH', CHROME_PUSH_PLUGIN_URL . 'assets/js/library/Dexie.min.js', $tmp_sw);
-	    $tmp_sw = str_replace('SUB_PATH', get_option('siteurl') . '/?regId=', $tmp_sw);
+	    $tmp_sw = str_replace('DEXIE_PATH', self::fixHttpsURL(CHROME_PUSH_PLUGIN_URL) . 'assets/js/library/Dexie.min.js', $tmp_sw);
+	    $tmp_sw = str_replace('SUB_PATH', get_option('siteurl') . '/?subId=', $tmp_sw);
 	    $tmp_sw = str_replace('ICON_PATH', get_option('web_push_icon'), $tmp_sw);
 	    $tmp_sw = str_replace('DEBUG_VAR', true, $tmp_sw);
 	    $form_url = 'admin.php?page=chrome-push';
@@ -246,7 +246,7 @@ class WPChromePush {
 	 */
 	public function registerPushJs() {
 	  	
-		wp_register_script( 'web-push', CHROME_PUSH_PLUGIN_URL . 'assets/js/push.js', array('jquery'), '1.0', true);
+		wp_register_script( 'web-push',$this->fixHttpsURL(CHROME_PUSH_PLUGIN_URL) . 'assets/js/push.js', array('jquery'), '1.0', true);
 
 		$data = array(
 			'sw_path' => get_option('siteurl') . '/sw.js',
@@ -454,6 +454,20 @@ class WPChromePush {
 	 */
 	public function checkSSL() {
 		return strpos(get_option('siteurl'), 'https://') !== false;
+	}
+
+	/**
+	 * Fix http to https
+	 * @param  [type] $url [description]
+	 * @return [type]      [description]
+	 */
+	public static function fixHttpsURL($url) {
+		// only fix if source URL starts with http://
+		if (stripos($url, 'http://') === 0) {
+			$url = str_replace('http://', 'https://', $url) ;
+		}
+
+		return $url;
 	}
 
 	/**
